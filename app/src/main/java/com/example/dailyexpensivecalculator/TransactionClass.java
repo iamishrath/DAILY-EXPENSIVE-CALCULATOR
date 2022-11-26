c5012d1823797390d09cc292a5c9f326e23720a9
iamishrath <iamishrathfathi@gmail.com>
2022-11-26 08:56:21 +0530

    DAILY EXPENSIVE CALCULATOR

---
+++ app/src/main/java/com/example/dailyexpensivecalculator/TransactionClass.java
@@ -0,0 +1,39 @@
+package com.example.dailyexpensivecalculator;
+
+public class TransactionClass {
+        private int amount;
+        private String message;
+        private boolean positive;
+
+        public TransactionClass(int amount, String message, boolean positive) {
+            this.amount = amount;
+            this.message = message;
+            this.positive = positive;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public void setAmount(int amount) {
+            this.amount = amount;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public boolean isPositive() {
+            return positive;
+        }
+
+        public void setPositive(boolean positive) {
+            this.positive = positive;
+        }
+    }
+
+
